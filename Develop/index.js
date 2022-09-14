b09c30056fef68c041e82f039d1e6b41c5afef18
drewrvaz <drewrvaz@gmail.com>
2022-09-14 13:49:49 -0400

    Wrote the template for the readme, fixed the prompt order, added a then function that is yet to be completed

--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,36 @@
 const inquirer = require("inquirer");
 const fs = require("fs"   );
 
+//Template structure to generate the README
+const generateReadMe = ({ title, description, installation, usage, license, contributing, tests, github, email }) => `# ${title}
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Description
+${description}
+## Installation
+${installation}
+## Usage
+${usage}
+## License
+${license}
+## Contributing
+${contributing}
+## Tests
+${tests}
+## Questions
+E-mail: ${email}
+GitHub: ${github}`;
+
 // TODO: Create an array of questions for user input
 // const questions = [];
-// Using inquirer prompts to generate quesitons for the user
+// Using inquirer prompts to generate quesitons for the user to input the necessary info for their README
 inquirer.prompt([
   {
     type: "input",
@@ -18,12 +45,7 @@ inquirer.prompt([
   },
   {
     type: "input",
-    message: "Provide a table of contents for your project.",
-    name: "tableOfContents",
-  },
-  {
-    type: "input",
-    message: "What are the steps require to install your project?",
+    message: "What are the steps required to install your project?",
     name: "installation",
   },
   {
@@ -32,15 +54,20 @@ inquirer.prompt([
     name: "usage",
   },
   {
-    type: "list",
-    message: "Which license would you like to have attached to your project?",
-    name: "license",
-    choices: ["The MIT License", "The GPL License", "Apache License", "GNU License", "N/A"]
+    type: "input",
+    message: "What are the guidelines for contributing to your project?",
+    name: "contributing",
   },
   {
     type: "input",
     message: "Are there any tests you would like to add? If so, please include instructions for how to run them.",
-    name: "github",
+    name: "tests",
+  },
+  {
+    type: "list",
+    message: "Which license would you like to have attached to your project?",
+    name: "license",
+    choices: ["The MIT License", "The GPL License", "Apache License", "GNU License", "N/A"]
   },
   {
     type: "input",
@@ -52,7 +79,9 @@ inquirer.prompt([
     message: "What is your email address?",
     name: "email",
   }
-])
+]).then((answers) => {
+
+})
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
