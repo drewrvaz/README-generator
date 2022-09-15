b018915539e50934b94ac8d252af54fb4e541c6a
drewrvaz <drewrvaz@gmail.com>
2022-09-15 12:36:59 -0400

    Finsihed wriitng what I thought would create a readme. It is currently broken, trying to trouble shoot

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
-const fs = require("fs"   );
+const fs = require("fs");
 
-//Template structure to generate the README
+// Template structure to generate the README
 const generateReadMe = ({ title, description, installation, usage, license, contributing, tests, github, email }) => `# ${title}
 
 ## Table of Contents
@@ -35,60 +35,69 @@ GitHub: ${github}`;
 inquirer.prompt([
   {
     type: "input",
-    message: "What is the title of your project?",
     name: "title",
+    message: "What is the title of your project?",
   },
   {
     type: "input",
-    message: "Provide a description of your project.",
     name: "description",
+    message: "Provide a description of your project.",
   },
   {
     type: "input",
-    message: "What are the steps required to install your project?",
     name: "installation",
+    message: "What are the steps required to install your project?",
   },
   {
     type: "input",
-    message: "What are the instructions for use of your project?",
     name: "usage",
+    message: "What are the instructions for use of your project?",
   },
   {
     type: "input",
-    message: "What are the guidelines for contributing to your project?",
     name: "contributing",
+    message: "What are the guidelines for contributing to your project?",
   },
   {
     type: "input",
-    message: "Are there any tests you would like to add? If so, please include instructions for how to run them.",
     name: "tests",
+    message: "Are there any tests you would like to add? If so, please include instructions for how to run them.",
   },
   {
     type: "list",
-    message: "Which license would you like to have attached to your project?",
     name: "license",
+    message: "Which license would you like to have attached to your project?",
     choices: ["The MIT License", "The GPL License", "Apache License", "GNU License", "N/A"]
   },
   {
     type: "input",
-    message: "What is your GitHub username?",
     name: "github",
+    message: "What is your GitHub username?",
   },
   {
     type: "input",
-    message: "What is your email address?",
     name: "email",
+    message: "What is your email address?",
   }
-]).then((answers) => {
-  const readMeContent = generateReadMe(answers);
+]).then((data) => {
+  // const readMeContent = generateReadMe(data);
 
-  fs.writefile("newREADME.md", readMeContent, (err) =>
-    err ? console.log(err) : console.log("Successfully generated ReadMe")
-  ); 
+  writeToFile();
+
+  // fs.writeFile(`newREADME.md`, readMeContent, (err) =>
+  //   err ? console.log(err) : console.log("Successfully generated ReadMe.md")
+  // ); 
 })
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+  const readMeContent = generateReadMe(data);
+  // const fileName = "newREADME.md";
+
+  fs.writeToFile(`newREADME.md`, readMeContent, (err) =>
+    err ? console.log(err) : console.log("Successfully generated ReadMe.md")
+  ); 
+}
 
 // TODO: Create a function to initialize app
 function init() {}
